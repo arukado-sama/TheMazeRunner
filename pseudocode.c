b704a32527ee40918934a3d033e0058c715dee87
Navarobirsk <Navarobirsk>
2014-12-19 10:11:04 +0000

    ajout fonction de scan et de mise en mémoire (forme du labyrinthe seulement)

--- pseudocode.c
+++ pseudocode.c
@@ -71,3 +71,34 @@ var pursuit(){ //joueur détecté, poursuite entamée
 	patchfind();
 	move();
 }
+
+var fieldMemorize(int coordJoueur[2], int maze[][], int mazeMemory[][]){ // le joueur scanne les environs depuis sa posistion actuelle et les mets en mémoires
+	//s'il ya un mur, il le met en mémoire et ne scanne pas au-délà
+	if( maze [coordJoueur[0]+1] [coordJoueur[1]] == 0) mazeMemory[coordJoueur[0]+1] [coordJoueur[1]]=0; else { //coté droit
+		mazeMemory[coordJoueur[0]+1] [coordJoueur[1]]=maze [coordJoueur[0]+1] [coordJoueur[1]]; //la case déjà testé dans le if, mais mis en mémoire
+		mazeMemory[coordJoueur[0]+1] [coordJoueur[1]+1]=maze[coordJoueur[0]+1] [coordJoueur[1]+1]; //en haut
+		mazeMemory[coordJoueur[0]+1] [coordJoueur[1]-1]=maze[coordJoueur[0]+1] [coordJoueur[1]-1]; //en bas
+		mazeMemory[coordJoueur[0]+1+1] [coordJoueur[1]]=maze[coordJoueur[0]+1+1] [coordJoueur[1]]; //à droite
+	}
+	
+	if( maze [coordJoueur[0]-1] [coordJoueur[1]] == 0) mazeMemory[coordJoueur[0]-1] [coordJoueur[1]]=0; else { //coté gauche
+		mazeMemory[coordJoueur[0]-1] [coordJoueur[1]]=maze [coordJoueur[0]-1] [coordJoueur[1]]; //la case déjà testé dans le if, mais mis en mémoire
+		mazeMemory[coordJoueur[0]-1] [coordJoueur[1]+1]=maze[coordJoueur[0]-1] [coordJoueur[1]+1]; //en haut
+		mazeMemory[coordJoueur[0]-1] [coordJoueur[1]-1]=maze[coordJoueur[0]-1] [coordJoueur[1]-1]; //en bas
+		mazeMemory[coordJoueur[0]-1-1] [coordJoueur[1]]=maze[coordJoueur[0]-1-1] [coordJoueur[1]]; //à gauche
+	}
+	
+	if( maze [coordJoueur[0]] [coordJoueur[1]+1] == 0) mazeMemory[coordJoueur[0]] [coordJoueur[1]+1]=0; else { //coté haut
+		mazeMemory[coordJoueur[0]] [coordJoueur[1]+1]=maze [coordJoueur[0]] [coordJoueur[1]+1]; //la case déjà testé dans le if, mais mis en mémoire
+		mazeMemory[coordJoueur[0]+1] [coordJoueur[1]+1]=maze[coordJoueur[0]+1] [coordJoueur[1]+1]; //à droite
+		mazeMemory[coordJoueur[0]-1] [coordJoueur[1]+1]=maze[coordJoueur[0]-1] [coordJoueur[1]+1]; //à gauche
+		mazeMemory[coordJoueur[0]] [coordJoueur[1]+1+1]=maze[coordJoueur[0]] [coordJoueur[1]+1+1]; //en haut
+	}
+	
+	if( maze [coordJoueur[0]] [coordJoueur[1]-1] == 0) mazeMemory[coordJoueur[0]] [coordJoueur[1]-1]=0; else { //coté haut
+		mazeMemory[coordJoueur[0]] [coordJoueur[1]-1]=maze [coordJoueur[0]] [coordJoueur[1]-1]; //la case déjà testé dans le if, mais mis en mémoire
+		mazeMemory[coordJoueur[0]+1] [coordJoueur[1]-1]=maze[coordJoueur[0]+1] [coordJoueur[1]-1]; //à droite
+		mazeMemory[coordJoueur[0]-1] [coordJoueur[1]-1]=maze[coordJoueur[0]-1] [coordJoueur[1]-1]; //à gauche
+		mazeMemory[coordJoueur[0]] [coordJoueur[1]-1-1]=maze[coordJoueur[0]] [coordJoueur[1]-1-1]; //en bas
+	}
+}
